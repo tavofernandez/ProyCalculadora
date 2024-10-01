8c883c8ba2f38507689960c7b2ac8d188a9cf5cc
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-30 23:12:57 -0500

    Agregamos el proyecto calculadora

---
+++ frmCalculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyCalculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+    }
+}
